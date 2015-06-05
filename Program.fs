21b0fac07dd43d1bbea529bdae7c13bece81bba5
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-06-05 21:09:31 +0200

    Hello World from Freya!

---
+++ Program.fs
@@ -0,0 +1,55 @@
+﻿open System
+open System.Text
+
+open Arachne.Http
+open Arachne.Language
+open Arachne.Uri.Template
+
+open Freya.Core
+open Freya.Machine
+open Freya.Machine.Extensions.Http
+open Freya.Machine.Extensions.Http.Cors
+open Freya.Machine.Router
+open Freya.Router
+
+open Microsoft.Owin.Hosting
+
+let inline write (text : string) =
+    freya {
+        return {
+            Data = Encoding.UTF8.GetBytes text
+            Description =
+                { Charset = Some Charset.Utf8
+                  Encodings = None
+                  MediaType = Some MediaType.Html
+                  Languages = Some [ LanguageTag.Parse "en" ] } } }
+
+let commonMediaTypes =
+    freya {
+        return [
+            MediaType.Html ] }
+
+let common =
+    freyaMachine {
+        using http
+        mediaTypesSupported commonMediaTypes }
+
+let home =
+    freyaMachine {
+        including common
+        methodsSupported ( freya { return [ GET ] } )
+        handleOk (fun _ -> freya { return! write "Hello World!" } ) } |> FreyaMachine.toPipeline
+
+let musicStore =
+    freyaRouter {
+        resource (UriTemplate.Parse "/") home } |> FreyaRouter.toPipeline
+
+type Project () =
+    member __.Configuration () =
+        OwinAppFunc.ofFreya musicStore
+
+[<EntryPoint>]
+let run _ =
+    let _ = WebApp.Start<Project> ("http://localhost:8080")
+    let _ = Console.ReadLine ()
+    0
