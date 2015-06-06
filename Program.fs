c2a812ab0ed7410eeb7ed216135b578ec8acc61c
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-06-06 19:49:50 +0200

    attach Site.css

--- Program.fs
+++ Program.fs
@@ -1,11 +1,14 @@
 ﻿open System
 open System.Text
+open System.Reflection
 
 open Arachne.Http
+open Arachne.Http.Cors
 open Arachne.Language
 open Arachne.Uri.Template
 
 open Freya.Core
+open Freya.Core.Operators
 open Freya.Machine
 open Freya.Machine.Extensions.Http
 open Freya.Machine.Extensions.Http.Cors
@@ -15,10 +18,26 @@ open Freya.Router
 
 open Microsoft.Owin.Hosting
 
-let inline write (text : string) =
+type Container = {
+    Greeting : string
+}
+
+open RazorEngine.Templating
+
+try 
+    let template = IO.File.ReadAllText("index.cshtml")
+    RazorEngine.Engine.Razor.Compile(template, "mylayout")
+with e ->
+    ()
+
+let inline write (view : string, model : 'a) =
     freya {
+        let contents = IO.File.ReadAllText(view + ".cshtml")
+        let result =
+            RazorEngine.Engine.Razor.RunCompile(contents, "templateKey", typeof<'a>, model)
+
         return {
-            Data = Encoding.UTF8.GetBytes text
+            Data = Encoding.UTF8.GetBytes result
             Description =
                 { Charset = Some Charset.Utf8
                   Encodings = None
@@ -38,7 +57,7 @@ let common =
 let getAlbum =
     freya {
         let! id = Freya.getLensPartial (Route.atom "id")
-        return! write (sprintf "Album no %d" (Int32.Parse id.Value))
+        return! write ("home", {Greeting = sprintf "Album no %d" (Int32.Parse id.Value) })
     }
 
 let albumMalformed = 
@@ -52,14 +71,14 @@ let albumMalformed =
 let getGenre =
     freya {
         let! name = Freya.getLensPartial (Route.atom "name")
-        return! write (sprintf "Genre: %s" name.Value)
+        return! write ("home", {Greeting = sprintf "Genre: %s" name.Value})
     }
 
 let home =
     freyaMachine {
         including common
         methodsSupported ( freya { return [ GET ] } )
-        handleOk (fun _ -> freya { return! write "Hello World!" } ) } |> FreyaMachine.toPipeline
+        handleOk (fun _ -> freya { return!  write ("home", {Greeting = "Hello World!" } ) } ) } |> FreyaMachine.toPipeline
 
 let album = 
     freyaMachine {
@@ -68,11 +87,11 @@ let album =
         methodsSupported ( freya { return [ GET ] } ) 
         handleOk (fun _ -> getAlbum) } |> FreyaMachine.toPipeline
 
-let albums = 
+let genres = 
     freyaMachine {
         including common
         methodsSupported ( freya { return [ GET ] } ) 
-        handleOk (fun _ -> freya { return! write "many albums!" } ) } |> FreyaMachine.toPipeline
+        handleOk (fun _ -> freya { return!  write ("home", {Greeting =  "many albums!" } ) } ) } |> FreyaMachine.toPipeline
 
 let genre =
     freyaMachine {
@@ -80,11 +99,62 @@ let genre =
         methodsSupported ( freya { return [ GET ] } )
         handleOk (fun _ -> getGenre ) } |> FreyaMachine.toPipeline
 
+let defaults =
+    freyaMachine {
+        using http
+        using httpCors
+
+        corsHeadersSupported (Freya.init [ "accept"; "content-type" ])
+        corsMethodsSupported (Freya.init [ GET; OPTIONS ])
+        corsOriginsSupported (Freya.init AccessControlAllowOriginRange.Any)
+
+        charsetsSupported (Freya.init [ Charset.Utf8 ])
+        languagesSupported (Freya.init [ LanguageTag.Parse "en" ]) }
+
+let private resourceAssembly =
+    Assembly.GetExecutingAssembly ()
+
+let resource key =
+    use stream = IO.File.OpenRead(key)
+    use reader = new IO.StreamReader (stream)
+
+    Encoding.UTF8.GetBytes (reader.ReadToEnd ())
+
+let private cssContent =
+    resource "Site.css"
+
+
+let private firstNegotiatedOrElse def =
+    function | Negotiated (x :: _) -> x
+             | _ -> def
+
+let represent n x =
+    { Description =
+        { Charset = Some (n.Charsets |> firstNegotiatedOrElse Charset.Utf8)
+          Encodings = None
+          MediaType = Some (n.MediaTypes |> firstNegotiatedOrElse MediaType.Text)
+          Languages = Some [ n.Languages |> firstNegotiatedOrElse (LanguageTag.Parse "en") ] }
+      Data = x }
+
+
+let private getContent content n =
+    represent n <!> Freya.init content
+
+let private getCss =
+    getContent cssContent
+
+let private css =
+    freyaMachine {
+        including defaults
+        mediaTypesSupported (Freya.init [ MediaType.Css ])
+        handleOk getCss } |> FreyaMachine.toPipeline
+
 let musicStore =
     freyaRouter {
+        resource (UriTemplate.Parse "/Site.css") css
         resource (UriTemplate.Parse "/") home
         resource (UriTemplate.Parse "/album/{id}") album
-        resource (UriTemplate.Parse "/albums") albums
+        resource (UriTemplate.Parse "/genres") genres
         resource (UriTemplate.Parse "/genre/{name}") genre } |> FreyaRouter.toPipeline
 
 type Project () =
@@ -93,6 +163,8 @@ type Project () =
 
 [<EntryPoint>]
 let run _ =
+    printfn "starting..."
     let _ = WebApp.Start<Project> ("http://localhost:8080")
+    printfn "app started. Press enter to quit"
     let _ = Console.ReadLine ()
     0
