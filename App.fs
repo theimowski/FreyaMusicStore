1b2361e8954a5d9ebb3297ef765cb2d9cc3fc1c2
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-06-07 11:53:15 +0200

    extract StaticFiles

--- App.fs
+++ App.fs
@@ -1,4 +1,4 @@
-﻿module FreyaMusicStore.Program
+﻿module FreyaMusicStore.App
 
 open System
 open System.IO
@@ -60,74 +60,6 @@ let genre =
         handleOk (fun _ -> getGenre ) } |> FreyaMachine.toPipeline
 
 
-let getFileInfo (path: string) =
-    let filePath = path.Trim ([| '/' |])
-    let fileInfo = FileInfo (filePath)
-
-    fileInfo
-
-let readFile (file: FileInfo) =
-    File.ReadAllBytes (file.FullName)
-
-// Response
-
-let fileTypes =
-    [ ".jpg", MediaType.Parse "image/jpeg"
-      ".png", MediaType.Parse "image/png"
-      ".gif", MediaType.Parse "image/gif"
-      ".css", MediaType.Css] |> Map.ofList
-
-let represent (n: Specification) x =
-    { Description =
-        { Charset = None
-          Encodings = None
-          MediaType = Some ((function | Negotiated x -> List.head x 
-                                      | _ -> MediaType.Text) n.MediaTypes)
-          Languages= None }
-      Data = x }
-
-// Freya
-
-let path =
-    Freya.memo (Freya.getLens Request.path)
-
-let fileInfo =
-    Freya.memo (getFileInfo <!> path)
-
-let file =
-    Freya.memo (readFile <!> fileInfo)
-
-let fileType =
-    Freya.memo ((function | (x: FileInfo) when x.Exists -> [ Map.find x.Extension fileTypes ]
-                          | _ -> [ MediaType.Text ]) <!> fileInfo)
-
-// Machine
-
-let existsDecision =
-    (fun (x: FileInfo) -> x.Exists) <!> fileInfo
-
-let fileHandler n =
-    represent n <!> file
-
-let lastModifiedConfiguration =
-    (fun (x: FileInfo) -> x.LastWriteTimeUtc) <!> fileInfo
-
-let mediaTypesConfiguration =
-    fileType
-
-// Resources
-
-let files : FreyaPipeline =
-    freyaMachine {
-        using http
-        methodsSupported (Freya.init [ GET; HEAD ])
-        mediaTypesSupported mediaTypesConfiguration
-        exists existsDecision
-        handleOk fileHandler } |> FreyaMachine.toPipeline
-
-
-
-
 let musicStore =
     freyaRouter {
         resource (UriTemplate.Parse "/") home
@@ -137,7 +69,7 @@ let musicStore =
 
 type Project () =
     member __.Configuration () =
-        OwinAppFunc.ofFreya (musicStore >?= files)
+        OwinAppFunc.ofFreya (musicStore >?= StaticFiles.pipe)
 
 [<EntryPoint>]
 let run _ =
